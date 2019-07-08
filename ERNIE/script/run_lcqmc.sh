567d752b94bf8e512e0581727bc3a537d0203854
xuzhou01 <xuzhou01@baidu.com>
2019-07-08 19:48:50 +0800

    run_lcqmc

--- ERNIE/script/run_lcqmc.sh
+++ ERNIE/script/run_lcqmc.sh
@@ -5,14 +5,16 @@ export CUDA_VISIBLE_DEVICES=0
 
 MODEL_PATH='./download/models'
 TASK_DATA_PATH='./download/task_data'
+PYTHON_PATH='~/paddle_1.3/paddle_release_home/python/bin/python'
 
-python -u run_classifier.py \
+${PYTHON_PATH} -u run_classifier.py \
                    --use_cuda false \
                    --verbose true \
-                   --do_train true \
-                   --do_val true \
+                   --do_train false \
+                   --do_val false \
                    --do_test true \
                    --batch_size 32 \
+                   --init_checkpoint ${MODEL_PATH}/params \
                    --init_pretraining_params ${MODEL_PATH}/params \
                    --train_set ${TASK_DATA_PATH}/lcqmc/train.tsv \
                    --dev_set ${TASK_DATA_PATH}/lcqmc/dev.tsv \
@@ -31,3 +33,7 @@ python -u run_classifier.py \
                    --num_iteration_per_drop_scope 1 \
                    --num_labels 2 \
                    --random_seed 1
+
+# Load model from ./download/models/params
+# Final test result:
+# [test evaluation] ave loss: 0.678016, ave acc: 0.575680, data_num: 12500, elapsed time: 2664.698558 s
